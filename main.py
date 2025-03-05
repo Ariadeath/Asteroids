43732ea3b2d7ab2f43ae126901e0c09df5b92f9e
Ariadeath <konradsgraeff@gmail.com>
2025-03-04 20:11:48 -0700

    Drew the player in-game

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
 import pygame 
 import constants
+import player
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0 # Delta time starts at 0 here
+    player_instance = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
     #Game loop
     running = True
@@ -17,11 +23,16 @@ def main():
         #Fills the screen with black
         screen.fill("black")
         
+        #Creates Player
+        player_instance.draw(screen)
+        
         #Updates the display
         pygame.display.flip()
 
         # Regulates FPS
         dt = clock.tick(60)/ 1000
+
+        
     
 
 if __name__ == "__main__":
