8b1f2829b2f616505a33e743891603f3e5894c18
Ariadeath <konradsgraeff@gmail.com>
2025-03-04 19:22:06 -0700

    Refined game loop and regulated FPS

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import pygame 
-from constants import *
+import constants
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 # Delta time starts at 0 here
+
     #Game loop
-    while True:
+    running = True
+    while running:
         #Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
 
         #Fills the screen with black
         screen.fill("black")
         
         #Updates the display
         pygame.display.flip()
+
+        # Regulates FPS
+        dt = clock.tick(60)/ 1000
     
 
 if __name__ == "__main__":
