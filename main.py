669fb5bb0e71501a6cca034a571610066497bdd7
Ariadeath <konradsgraeff@gmail.com>
2025-03-04 21:22:34 -0700

    Finished normal movement

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0 # Delta time starts at 0 here
+    
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    # Set containers
+    player.Player.containers = (updatable, drawable)
+    
+    # Create a single player instance
     player_instance = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
     #Game loop
@@ -21,11 +30,12 @@ def main():
                 running = False
 
         #Update game state
-        player_instance.update(dt) # Where to hook in update method
+        updatable.update(dt) # Where to hook in update method
         
         #Render
         screen.fill("black")
-        player_instance.draw(screen)
+        for drawable_obj in drawable:
+            drawable_obj.draw(screen)
         
         #Updates the display
         pygame.display.flip()
